ecde83a96729aab3201236aa6de078843a1fc92a
Durantnt <53827734@qq.com>
2020-09-18 21:01:23 +0800

    登录校验已完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,25 @@
 <template>
- <div class="login">
-  <div class="loginwarp">
-    <div class="loginlog">
-      <img src="./logo_index.png" alt="">
+  <div class="login">
+    <div class="loginwarp">
+      <div class="loginlog">
+        <img src="./logo_index.png" alt="" />
+      </div>
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-form-item prop="mobile">
+          <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="loginbtn" type="primary" @click="login"
+            >登录</el-button
+          >
+          <el-button>获取验证码</el-button>
+        </el-form-item>
+      </el-form>
     </div>
-  <el-form ref="form" :model="form">
-    <el-form-item>
-      <el-input v-model="form.name" placeholder="请输入手机号" ></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-input v-model="form.name" placeholder="请输入验证码"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button class="loginbtn" type="primary">登录</el-button>
-      <el-button>获取验证码</el-button>
-    </el-form-item>
-  </el-form>
   </div>
- </div>
 </template>
 
 <script>
@@ -25,8 +27,31 @@ export default {
   data () {
     return {
       form: {
-        name: ''
+        mobile: '',
+        code: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度为11位的数字', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 6, max: 6, message: '长度为6位的数字', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     }
   }
 }
