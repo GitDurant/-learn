a5522a66f116609a5aed0181fac3e0ddf55d85ff
Durantnt <53827734@qq.com>
2020-09-18 11:20:18 +0800

    learn项目的初期

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+  <h1>我是根组件APP</h1>
+  <router-link to="./views/useElement"></router-link>
+  <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
---
+++ src/views/useElement/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="Element">
+    <h2>ElementUI的简单使用</h2>
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+  return {
+    radio: '1'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
